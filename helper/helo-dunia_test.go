155a785ed2653ab20dfb1a4609ed6b0a916b64c1
Rizky Riyadi <rizkyriady16@gmail.com>
2022-12-16 13:10:33 +0700

    update

--- helper/helo-dunia_test.go
+++ helper/helo-dunia_test.go
@@ -1,6 +1,11 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Yamdi")
@@ -9,4 +14,20 @@ func TestHelloWorld(t *testing.T) {
 		// error
 		t.Error("Harusnya Yamdi, Selamat datang")
 	}
+}
+
+func TestHelloWorldAsstert(t *testing.T) {
+	result := HelloWorld("Gaji")
+
+	assert.Equal(t, "Gaji, Selamat datangg", result)
+	fmt.Println("End of TestHelloWorldAssert")
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("BEFORE TEST BOSKU")
+
+	m.Run()
+
+	fmt.Println("AFTER TEST BOSKU")
+
 }
